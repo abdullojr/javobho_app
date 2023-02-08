533b47075613f182ba004783d73dd695ce172d99
abdullojr <iamnozimjon@gmail.com>
2023-02-08 15:14:08 +0500

    +

--- src/Components/leftbar/index.tsx
+++ src/Components/leftbar/index.tsx
@@ -5,6 +5,7 @@ import Icon from "Components/icon";
 import { NavLink } from "react-router-dom";
 import { NavBtn } from "./components/route-btn";
 import IRouteBtn from "./components/route-btn/IRouteBtn";
+import img from "assets/img/profile.png";
 
 const LeftBar = (props: ILeftBar.props) => {
   const nav: IRouteBtn.navProps[] = [
@@ -48,6 +49,30 @@ const LeftBar = (props: ILeftBar.props) => {
             <Icon name="arrowLongLeft" />
           </div>
         </div>
+
+        <div className={"account-full-info"}>
+          <div>
+            <img
+              src={img}
+              alt="profile_img"
+              width={32}
+              className="profile_img"
+            />
+          </div>
+          <div className="username">
+            <h3>Nozimjon</h3>
+            <b>@{"username"}</b>
+          </div>
+          <div className="follow-info">
+            <span>
+              <b>{64}</b> <b className="simple_text">Following</b>
+            </span>
+            <span>
+              <b>{18}</b> <b className="simple_text">Follower</b>
+            </span>
+          </div>
+        </div>
+
         <div className="nav-routes">
           {nav.map(item => (
             <NavLink to={item.path} className={"link"}>
