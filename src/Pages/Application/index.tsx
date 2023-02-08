82bcc348f7d024761b3cc33eb0d896d95f4172f1
abdullojr <iamnozimjon@gmail.com>
2023-02-08 15:35:14 +0500

    plus

--- src/Pages/Application/index.tsx
+++ src/Pages/Application/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Application = () => {
   return (
-    <>
+    <div>
       <h1>appllication 1</h1>
       <h1>appllication</h1>
       <h1>appllication</h1>
@@ -24,7 +24,7 @@ const Application = () => {
       <h1>appllication</h1>
       <h1>appllication</h1>
       <h1>appllication0</h1>
-    </>
+    </div>
   );
 };
 
